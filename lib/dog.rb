921c24954be2fa550071480975a4e20fecf9dd10
Isaac Villicana <villicana.i@gmail.com>
2018-04-13 20:35:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,15 @@ class Dog
   attr_accessor :name
 
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
 end
